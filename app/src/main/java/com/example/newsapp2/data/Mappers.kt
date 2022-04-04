cfab1a8a975b519831609a5708ff3767454192f3
Lisenblsh <lemi013794@gmail.com>
2022-04-04 15:01:02 +0700

    Добавлена функция для обратного преобразовавния из БД в API

--- app/src/main/java/com/example/newsapp2/data/Mappers.kt
+++ app/src/main/java/com/example/newsapp2/data/Mappers.kt
@@ -1,6 +1,7 @@
 package com.example.newsapp2.data
 
 import com.example.newsapp2.data.network.Articles
+import com.example.newsapp2.data.network.Source
 import com.example.newsapp2.data.room.ArticlesDB
 import com.example.newsapp2.data.room.TypeArticles
 
@@ -15,5 +16,17 @@ internal fun Articles.toArticlesDto(typeArticles: TypeArticles): ArticlesDB {
         urlToImage,
         publishedAt,
         typeArticles
+    )
+}
+
+internal fun ArticlesDB.toArticles(): Articles {
+    return Articles(
+        Source(source),
+        author,
+        title,
+        description,
+        url,
+        urlToImage,
+        publishedAt
     )
 }
